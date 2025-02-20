d49b4a5e00d01026a64045990d20f4feb095a615
Vasty Muaku <vasty@wethinkcode.co.za>
2025-02-20 15:37:41 +0200

    added search component

--- mock-stock/src/App.tsx
+++ mock-stock/src/App.tsx
@@ -1,12 +1,40 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
-  return (
+function SearchBar(){
+  return(
     <>
-      <h1>Hello, World!</h1>
+      <form id='form-search' name='for-search'>
+        <label htmlFor='search_bar'>Search</label><br/>
+        <input type='text' id='search_bar' name='search_bar'></input>
+        <input id='submit' type='submit' value='submit'></input>
+        <br/> <br/>
+        <input type='checkbox' id='in_stock' name='in_stock'></input>
+        <label htmlFor='in_stock'> Only show product in stock</label>
+      </form>
     </>
   )
 }
 
-export default App
+function ResultsTable(){
+  return(<>
+    <table>
+      <tr>
+        <th>Name</th>
+        <th>Price</th>
+      </tr>
+    </table>
+  </>)
+}
+
+export default function App() {
+  return (
+    <>
+      <h1>Mock Stock!</h1>
+      <br/><br/>
+      <SearchBar/>
+      <br/><br/>
+      <ResultsTable/>
+    </>
+  )
+}
